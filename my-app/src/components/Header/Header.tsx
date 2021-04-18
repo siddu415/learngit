6706366dd78ed3c2950444dcaffb913736904280
sid <siddarthareddy15@gmail.com>
2021-04-17 21:25:26 -0400

    reach router adds

---
+++ my-app/src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+// example src/layout.js
+import { Button } from 'theme-ui'
+
+
+const Header: React.FC = () => {
+    return (
+        <Button variant="buttons.primary">
+            This is Header
+        </Button>
+    )
+}
+
+
+export default Header;
